4a72c072073a381dfd3f17243b9a186badaa1cfc
Wojtek Wrona <w.wrona@selleo.com>
2016-02-23 23:30:14 +0100

    Move cell values from ids to js

--- javascript.js
+++ javascript.js
@@ -1,35 +1,43 @@
 $(function() {
-  var fields = $( '.field' ), currentPlayer = 1, playersScore = [0, 0];
+  var fields = $( '.field' ), currentPlayer = 1, playersScore = [0, 0], played = 0;
 
-  var reset = function() {
-    playersScore = [0, 0];
-    fields.html('');
+  var start = function() {
+    var value = 1;
+    $('td').each(function() {
+      this.value = value;
+      value *= 2;
+    })
   }
+  start();
 
   fields.on("click", function(event) {
+    if($(this).html() !== "")
+      return;
+
     if(currentPlayer == 1)
       $(this).html('X');
     else
       $(this).html('O');
 
-    var value = parseInt($(this).attr('id'));
-    playersScore[currentPlayer] += value;
+    playersScore[currentPlayer] += this.value;
     checkWinner(playersScore[currentPlayer]);
     changePlayer();
   })
-
-  var changePlayer = function() {
-    if (currentPlayer == 1)
-      currentPlayer = 0;
-    else
-      currentPlayer = 1;
-  }
 
   var checkWinner = function(score) {
     if(winning(score)){
       alert("Player " + currentPlayer + " won!");
       reset();
     }
+  }
+
+  var changePlayer = function() {
+    currentPlayer = Number(!currentPlayer);
+  }
+
+  var reset = function() {
+    playersScore = [0, 0];
+    fields.html('');
   }
 
   var winning = function(score) {
